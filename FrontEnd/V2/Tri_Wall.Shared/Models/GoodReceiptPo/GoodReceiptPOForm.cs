5d1883f15ca7e992a8ca83f895ce5b6dd881eee9
TengKimleang <Nonametotell4@gmail.com>
2024-05-13 10:40:00 +0700

    Add

---
+++ FrontEnd/V2/Tri_Wall.Shared/Models/GoodReceiptPo/GoodReceiptPOForm.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Tri_Wall.Shared.Models.GoodReceiptPo;
+
+public class GoodReceiptPOForm
+{
+    public string CardCode { get; set; }= string.Empty;
+    [Required(ErrorMessage ="Can not be empty")]
+    public List<GoodReceiptPOLine>? Lines { get; set; }
+}
+
+public class GoodReceiptPOLine
+{
+    public string ItemCode { get; set; } = string.Empty;
+}
